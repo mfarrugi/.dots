f68614680b9087a110955ede3fcc40f14d8f0caa
Marco Farrugia <afmarco@gmail.com>
2022-06-20 19:20:13 -0400

    zsh :: Cleanup aliases.

--- zsh/_aliases.zsh
+++ zsh/_aliases.zsh
@@ -1,25 +1,24 @@
 # Meta, add an alias to the end of this file.
 adda() { echo "alias $1='$2'" >> $DOTS/zsh/_aliases.zsh && source ~/.zshrc; }
 
-alias top='htop'
+alias h="history -i"
 alias mv='mv -iv'
-alias x='exit'
 alias trw='tmux rename-window '
-alias du='du -h --max-depth=1'
-alias g='git'
-
-alias df="df -h"
 
 # exa replaces `ls` and `tree`
-alias ls='exa'
-alias la='exa -la' # Override omzsh common-aliases because exa doesn't take the same flags.
-alias tree='exa --tree'
+alias ls='exa --sort=type --time-style=long-iso --group --icons'
+alias la='ls -la' # Override omzsh common-aliases because exa doesn't take the same flags.
+alias tree='ls --tree'
 
 journal() { vim "$HOME/notes/journal/$(date +%Y-%m-%d).md" }
 
 alias e='nvim'
 
 alias rg="rg --type-add 'bazel:*.bzl' --type-add 'bazel:*BUILD*'" 
+alias rgf='rg --files'
 
 alias -g P='| fpp'
 alias -g F='| fzf'
+alias black='black -S -l120'
+alias ef='e $(fzf)'
+alias kurl='curl -u: --negotiate'
